8afe9a9446a27046f5fed2cbe56f79580244acd8
Suyesha Chaudhary <suyeshaachaudhry@gmail.com>
2021-08-27 12:38:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Route, Switch } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
+function App() {
+  return (
+    <div>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Switch>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
+          <Route path="/products" exact>
+            <Products />
+          </Route>
+          <Route path="/products/:productId">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
+
+export default App;
